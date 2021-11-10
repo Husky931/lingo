58b29c0b80ca78f67404dad6ec56636460d9e14a
Xz33M Hask <pecevgligor@protonmail.com>
2021-11-10 14:27:53 +0100

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 6321;
+const PORT = 80;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
